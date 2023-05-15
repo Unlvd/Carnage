82aebf8dfefe99ad3c57cbcad5a5fd32f8b6a45a
Sveta <i2pac2@gmail.com>
2023-05-15 11:42:20 +1000

    Add checkBox

--- app/src/main/java/com/example/carnage/MainActivity.kt
+++ app/src/main/java/com/example/carnage/MainActivity.kt
@@ -3,9 +3,33 @@ package com.example.carnage
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 
+import android.widget.CheckBox
+import com.example.carnage.databinding.ActivityMainBinding
+
 class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
+    private var checkedBox: CheckBox? = null
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        selectCheckBox(binding.cbHead)
+        selectCheckBox(binding.cbChest)
+        selectCheckBox(binding.cbBelt)
+        selectCheckBox(binding.cbLegs)
     }
+
+
+    private fun selectCheckBox(checkBox: CheckBox) {
+        checkBox.setOnClickListener {
+            checkedBox?.isChecked = false
+            checkedBox = checkBox
+            checkedBox?.isChecked = true
+        }
+
+    }
+    
 }
